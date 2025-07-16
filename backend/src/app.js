4fe5ffe405f50c51c029a4b6fdbbb70b072a8355
Anmol Kumar <anmolkumar.cod01@gmail.com>
2025-07-16 23:28:54 +0530

    added frontend deployed link in cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://mailsync-one.vercel.app'],
     credentials: true
 }))
 
